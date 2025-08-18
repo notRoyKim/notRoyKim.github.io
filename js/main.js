a6456bc67fc4bba5d27aa09bb1e4b513dc21625f
NRK <49558826+notRoyKim@users.noreply.github.com>
2025-08-18 12:14:34 +0900

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+async function loadHTML(id, file) {
+  const res = await fetch(file);
+  document.getElementById(id).innerHTML = await res.text();
+}
+
+async function initPage() {
+  await loadHTML("header-placeholder", "header.html");
+  await loadHTML("footer-placeholder", "footer.html");
+
+  const menuBtn = document.getElementById("menuBtn");
+  const lnb = document.getElementById("lnb");
+  const contents = document.getElementById("contents");
+
+  if(menuBtn) {
+    menuBtn.addEventListener("click", () => {
+      lnb.classList.toggle("active");
+      if(contents) contents.classList.toggle("shrink");
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", initPage);
