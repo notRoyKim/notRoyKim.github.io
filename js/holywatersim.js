13fe774f5b2eea9614a8b3e029d044258294db23
NRK <49558826+notRoyKim@users.noreply.github.com>
2025-09-11 12:19:39 +0900

    Update holywatersim.js

--- js/holywatersim.js
+++ js/holywatersim.js
@@ -1,112 +1,9 @@
-const id72001 = { min: 0, max: 10, value: "최대공격력" };
-const id72002 = { min: 10, max: 20, value: "최대공격력" };
-const id72003 = { min: 20, max: 30, value: "최대공격력" };
-const id72004 = { min: 30, max: 40, value: "최대공격력" };
-const id72005 = { min: 40, max: 50, value: "최대공격력" };
-const id72006 = { min: 10, max: 20, value: 0 };
-const id72007 = { min: 10, max: 20, value: 0 };
-const id72008 = { min: 10, max: 20, value: 0 };
-const id72009 = { min: 10, max: 20, value: 0 };
-const id72010 = { min: 10, max: 20, value: 0 };
-const id72011 = { min: 10, max: 20, value: 0 };
-const id72012 = { min: 10, max: 20, value: 0 };
-const id72013 = { min: 10, max: 20, value: 0 };
-const id72014 = { min: 10, max: 20, value: 0 };
-const id72015 = { min: 10, max: 20, value: 0 };
-const id72016 = { min: 10, max: 20, value: 0 };
-const id72017 = { min: 10, max: 20, value: 0 };
-const id72018 = { min: 10, max: 20, value: 0 };
-const id72019 = { min: 10, max: 20, value: 0 };
-const id72020 = { min: 10, max: 20, value: 0 };
-const id72021 = { min: 10, max: 20, value: 0 };
-const id72022 = { min: 10, max: 20, value: 0 };
-const id72023 = { min: 10, max: 20, value: 0 };
-const id72024 = { min: 10, max: 20, value: 0 };
-const id72025 = { min: 10, max: 20, value: 0 };
-const id72026 = { min: 10, max: 20, value: 0 };
-const id72027 = { min: 10, max: 20, value: 0 };
-const id72028 = { min: 10, max: 20, value: 0 };
-const id72029 = { min: 10, max: 20, value: 0 };
-const id72030 = { min: 10, max: 20, value: 0 };
-const id72031 = { min: 10, max: 20, value: 0 };
-const id72032 = { min: 10, max: 20, value: 0 };
-const id72033 = { min: 10, max: 20, value: 0 };
-const id72034 = { min: 10, max: 20, value: 0 };
-const id72035 = { min: 10, max: 20, value: 0 };
-const id72036 = { min: 10, max: 20, value: 0 };
-const id72037 = { min: 10, max: 20, value: 0 };
-const id72038 = { min: 10, max: 20, value: 0 };
-const id72039 = { min: 10, max: 20, value: 0 };
-const id72040 = { min: 10, max: 20, value: 0 };
-const id72041 = { min: 10, max: 20, value: 0 };
-const id72042 = { min: 10, max: 20, value: 0 };
-const id72043 = { min: 10, max: 20, value: 0 };
-const id72044 = { min: 10, max: 20, value: 0 };
-const id72045 = { min: 10, max: 20, value: 0 };
-const id72046 = { min: 10, max: 20, value: 0 };
-const id72047 = { min: 10, max: 20, value: 0 };
-const id72048 = { min: 10, max: 20, value: 0 };
-const id72049 = { min: 10, max: 20, value: 0 };
-const id72050 = { min: 10, max: 20, value: 0 };
-const id72051 = { min: 10, max: 20, value: 0 };
-const id72052 = { min: 10, max: 20, value: 0 };
-const id72053 = { min: 10, max: 20, value: 0 };
-const id72054 = { min: 10, max: 20, value: 0 };
-const id72055 = { min: 10, max: 20, value: 0 };
-const id72056 = { min: 10, max: 20, value: 0 };
-const id72057 = { min: 10, max: 20, value: 0 };
-const id72058 = { min: 10, max: 20, value: 0 };
-const id72059 = { min: 10, max: 20, value: 0 };
-const id72060 = { min: 10, max: 20, value: 0 };
-const id72061 = { min: 10, max: 20, value: 0 };
-const id72062 = { min: 10, max: 20, value: 0 };
-const id72063 = { min: 10, max: 20, value: 0 };
-const id72064 = { min: 10, max: 20, value: 0 };
-const id72065 = { min: 10, max: 20, value: 0 };
-const id72066 = { min: 10, max: 20, value: 0 };
-const id72067 = { min: 10, max: 20, value: 0 };
-const id72068 = { min: 10, max: 20, value: 0 };
-const id72069 = { min: 10, max: 20, value: 0 };
-const id72070 = { min: 10, max: 20, value: 0 };
-const id72071 = { min: 10, max: 20, value: 0 };
-const id72072 = { min: 10, max: 20, value: 0 };
-const id72073 = { min: 10, max: 20, value: 0 };
-const id72074 = { min: 10, max: 20, value: 0 };
-const id72075 = { min: 10, max: 20, value: 0 };
-const id72076 = { min: 10, max: 20, value: 0 };
-const id72077 = { min: 10, max: 20, value: 0 };
-const id72078 = { min: 10, max: 20, value: 0 };
-const id72079 = { min: 10, max: 20, value: 0 };
-const id72080 = { min: 10, max: 20, value: 0 };
-const id72081 = { min: 10, max: 20, value: 0 };
-const id72082 = { min: 10, max: 20, value: 0 };
-const id72083 = { min: 10, max: 20, value: 0 };
-const id72084 = { min: 10, max: 20, value: 0 };
-const id72085 = { min: 10, max: 20, value: 0 };
-const id72086 = { min: 10, max: 20, value: 0 };
-const id72087 = { min: 10, max: 20, value: 0 };
-const id72088 = { min: 10, max: 20, value: 0 };
-const id72089 = { min: 10, max: 20, value: 0 };
-const id72090 = { min: 10, max: 20, value: 0 };
-const id72091 = { min: 10, max: 20, value: 0 };
-const id72092 = { min: 10, max: 20, value: 0 };
-const id72093 = { min: 10, max: 20, value: 0 };
-const id72094 = { min: 10, max: 20, value: 0 };
-const id72095 = { min: 10, max: 20, value: 0 };
-const id72096 = { min: 10, max: 20, value: 0 };
-const id72097 = { min: 10, max: 20, value: 0 };
-const id72098 = { min: 10, max: 20, value: 0 };
-const id72099 = { min: 10, max: 20, value: 0 };
-const id72100 = { min: 10, max: 20, value: 0 };
-const id72101 = { min: 10, max: 20, value: 0 };
-const id72102 = { min: 10, max: 20, value: 0 };
-
-
+const enchStruct = {
+  id72001 : { min: 0, max: 10, value: "최대공격력" },
+  id72002 : { min: 0, max: 10, value: "최대공격력" }
+}
 
 function enchantFunc(idObj) {
-
-  console.log("clicked")
-  
   const min = idObj.min;
   const max = idObj.max;
   return idObj.value + Math.floor(Math.random() * (max - min + 1)) + min;
@@ -115,8 +12,7 @@ function enchantFunc(idObj) {
 
 document.querySelector('.spin-btn').addEventListener('click', () => {
   const enchantID = Math.floor(Math.random() * (4)) + 72001;
-  console.log("id" + enchantID)
-  const result = enchantFunc("id" + enchantID);
+  const result = enchantFunc(enchStruct["id" + enchantID]);
   
   console.log(result)
   
