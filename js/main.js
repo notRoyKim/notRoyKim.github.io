2fd99c27d5b921ae8ee2fb7af1ef3f44f3d691d4
NRK <49558826+notRoyKim@users.noreply.github.com>
2025-09-11 11:24:41 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@ async function initPage() {
   await loadHTML("header-placeholder", "/header.html");
   await loadHTML("footer-placeholder", "/footer.html");
   
-  const uladh = document.querySelector(".uladh-placeholder");
+  const uladh = document.querySelector("#uladh-placeholder");
   if(uladh) await loadHTML("uladh-placeholder", "/uladh.html");
   
-  const holywater = document.querySelector(".holywater-placeholder");
+  const holywater = document.querySelector("#holywater-placeholder");
   if(holywater) await loadHTML("holywater-placeholder", "/holywater.html");
 
   const menuBtn = document.getElementById("menuBtn");
