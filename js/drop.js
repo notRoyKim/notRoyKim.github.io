0facb250bc9815bfa912dfb7be34813b8668d026
NRK <49558826+notRoyKim@users.noreply.github.com>
2025-09-18 13:53:50 +0900

    fix : drop feat update

--- js/drop.js
+++ js/drop.js
@@ -1,15 +1,30 @@
 const tech = [
-    {itemname:'패러시우스 위스퍼링 애로우', price:0, imgUrl: ''},
-    {itemname:'빛나는 구슬', price:0, imgUrl: ''},
-    {itemname:'각성된 힘의 결정', price:0, imgUrl: ''},
-    {itemname:'원혼이 깃든 연금술 결정', price:0, imgUrl: ''},
-    {itemname:'단단하게 결정화된 광물 조각', price:0, imgUrl: ''},
+    {itemname:'패러시우스 위스퍼링 애로우', price:0, imgUrl: '/assets/images/1420005.png'},
+    {itemname:'빛나는 구슬', price:0, imgUrl: '/assets/images/00002.png'},
+    {itemname:'각성된 힘의 결정', price:0, imgUrl: '/assets/images/64644.png'},
+    {itemname:'미지의 파편', price:0, imgUrl: '/assets/images/64108.png'},
+    {itemname:'오묘한 파편', price:0, imgUrl: '/assets/images/64112.png'},
+    {itemname:'빛바랜 파편', price:0, imgUrl: '/assets/images/64102.png'},
+    {itemname:'원혼이 깃든 연금술 결정', price:0, imgUrl: '/assets/images/64124.png'},
+    {itemname:'원혼이 깃든 칼날', price:0, imgUrl: '/assets/images/64125.png'},
+    {itemname:'원혼이 깃든 고목 조각', price:0, imgUrl: '/assets/images/64126.png'},
+    {itemname:'단단하게 결정화된 광물 조각', price:0, imgUrl: '/assets/images/64105.png'},
+    {itemname:'날카롭게 결정화된 광물 조각', price:0, imgUrl: '/assets/images/64109.png'},
+    {itemname:'기아스 데버스테이션이 깃든 결정', price:0, imgUrl: '/assets/images/64104.png'},
+    {itemname:'기아스 크러스티가 깃든 결정', price:0, imgUrl: '/assets/images/64103.png'},
+    {itemname:'기아스 코어', price:0, imgUrl: '/assets/images/64113.png'},
 ]
 
 document.addEventListener('buttonsInserted', () => {
     const buttons = document.querySelectorAll('.drop-btn');
     buttons.forEach(btn => {
         const index = parseInt(btn.value);
+
+        const img = document.createElement('img');
+        img.className = 'drop-btn-image';
+        img.src = tech[index].imgUrl;
+        btn.appendChild(img);
+
         const div = document.createElement('div');
         div.className = 'drop-btn-itemname';
         div.textContent = tech[index].itemname;
@@ -24,9 +39,9 @@ document.addEventListener('buttonsInserted', () => {
                 btn.classList.add('btn-active');
             }
 
-            const firstChild = btn.firstElementChild;
-            if (firstChild) {
-                navigator.clipboard.writeText(firstChild.textContent)
+            const secondChild = btn.children[1];
+            if (secondChild) {
+                navigator.clipboard.writeText(secondChild.textContent)
                     .then(() =>  showToast('🥴 클립보드에 복사되었습니다!'))
                     .catch(err =>  showToast('복사 실패 😢'));
             }
